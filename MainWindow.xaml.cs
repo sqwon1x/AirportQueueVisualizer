392bf1fdc510a4846547be541f64fc0b6041ee32
sqwon1x <c.hutych.mykhailo@student.uzhnu.edu.ua>
2026-04-14 20:47:09 +0300

    realization of the second stage

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,52 +12,26 @@ using System.Collections.ObjectModel;
 
 namespace AirportQueueVisualizer
 {
-    public class Passenger
-    {
-        public string Name { get; set; }
-        public string Flight { get; set; }
-    }
-
     public partial class MainWindow : Window
     {
-        ObservableCollection<Passenger> passengers = new ObservableCollection<Passenger>();
-
         public MainWindow()
         {
             InitializeComponent();
-            PassengerList.ItemsSource = passengers;
+            MainFrame.Navigate(new HomePage());
+        }
+        private void BtnHome_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new HomePage());
         }
 
-        private void Add_Click(object sender, RoutedEventArgs e)
+        private void BtnQueue_Click(object sender, RoutedEventArgs e)
         {
-            if (string.IsNullOrWhiteSpace(NameBox.Text) || string.IsNullOrWhiteSpace(FlightBox.Text))
-            {
-                MessageBox.Show("Будь ласка, введіть ім'я та номер рейсу!");
-                return;
-            }
-
-            passengers.Add(new Passenger
-            {
-                Name = NameBox.Text.Trim(),
-                Flight = FlightBox.Text.Trim(),
-            });
-
-            NameBox.Clear();
-            FlightBox.Clear();
-
-            NameBox.Focus();
+            MainFrame.Navigate(new QueuePage());
         }
 
-        private void Delete_Click(object sender, RoutedEventArgs e)
+        private void BtnInfo_Click(object sender, RoutedEventArgs e)
         {
-            if (PassengerList.SelectedItem is Passenger selectedPassenger)
-            {
-                passengers.Remove(selectedPassenger);
-            }
-            else
-            {
-                MessageBox.Show("Будь ласка, виберіть пасажира зі списку для видалення.");
-            }
+            MainFrame.Navigate(new InfoPage());
         }
     }
 }
